91d9182c8a02ca9652cf76ea5179eee66025017e
waheec19 <waheechowdhury80@gmail.com>
2023-03-14 22:37:26 -0500

    push to heroku

---
+++ client/src/js/database.js
@@ -0,0 +1,57 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+// We are creating a new database named 'jate' which will be using version 1 of the database.
+  openDB('jate', 1, {
+    // Add our database schema if it has not already been initialized.
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      // Create a new object store for the data and give it a key name of 'id' which needs to increment automatically.
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.error('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+
+  // Use the .put method to update the data in the database.
+  const request = store.put({ id: 1, value: content });
+  
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  // Create a connection to the database and version we want to use.
+  const jateDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+  result
+    ? console.log('Data retrieved from database')
+    : console.log('Data not found in the database.');
+
+  return result?.value;
+};
+
+initdb();
